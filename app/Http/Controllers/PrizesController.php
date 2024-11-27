e0e63b4269aef333c82754b97ceb7e8ad1671bcd
DESKTOP-G1AM2J3\Sushant <sushantgurjar4@gmail.com>
2024-11-27 22:01:40 +0530

    Initial commit for the new project

---
+++ app/Http/Controllers/PrizesController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\Prize;
+use App\Http\Requests\PrizeRequest;
+use Illuminate\Http\Request;
+
+
+
+class PrizesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $prizes = Prize::all();
+
+        return view('prizes.index', ['prizes' => $prizes]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        return view('prizes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  PrizeRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(PrizeRequest $request)
+    {
+        $prize = new Prize;
+        $prize->title = $request->input('title');
+        $prize->probability = floatval($request->input('probability'));
+        $prize->save();
+
+        return to_route('prizes.index');
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function edit($id)
+    {
+        $prize = Prize::findOrFail($id);
+        return view('prizes.edit', ['prize' => $prize]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  PrizeRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(PrizeRequest $request, $id)
+    {
+        $prize = Prize::findOrFail($id);
+        $prize->title = $request->input('title');
+        $prize->probability = floatval($request->input('probability'));
+        $prize->save();
+
+        return to_route('prizes.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $prize = Prize::findOrFail($id);
+        $prize->delete();
+
+        return to_route('prizes.index');
+    }
+
+
+    public function simulate(Request $request)
+    {
+
+
+        for ($i = 0; $i < $request->number_of_prizes ?? 10; $i++) {
+            Prize::nextPrize();
+        }
+
+        return to_route('prizes.index');
+    }
+
+    public function reset()
+    {
+        // TODO : Write logic here
+        return to_route('prizes.index');
+    }
+}
---
+++ resources/views/prizes/index.blade.php
@@ -0,0 +1,102 @@
+@extends('default')
+
+@section('content')
+
+
+    @include('prob-notice')
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-end mb-3">
+                    <a href="{{ route('prizes.create') }}" class="btn btn-info">Create</a>
+                </div>
+                <h1>Prizes</h1>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Probability</th>
+                            <th>Awarded</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($prizes as $prize)
+                            <tr>
+                                <td>{{ $prize->id }}</td>
+                                <td>{{ $prize->title }}</td>
+                                <td>{{ $prize->probability }}</td>
+                                <td>0</td>
+                                <td>
+                                    <div class="d-flex gap-2">
+                                        <a href="{{ route('prizes.edit', [$prize->id]) }}" class="btn btn-primary">Edit</a>
+                                        {!! Form::open(['method' => 'DELETE', 'route' => ['prizes.destroy', $prize->id]]) !!}
+                                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                                        {!! Form::close() !!}
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Simulate</h3>
+                    </div>
+                    <div class="card-body">
+                        {!! Form::open(['method' => 'POST', 'route' => ['simulate']]) !!}
+                        <div class="form-group">
+                            {!! Form::label('number_of_prizes', 'Number of Prizes') !!}
+                            {!! Form::number('number_of_prizes', 50, ['class' => 'form-control']) !!}
+                        </div>
+                        {!! Form::submit('Simulate', ['class' => 'btn btn-primary']) !!}
+                        {!! Form::close() !!}
+                    </div>
+
+                    <br>
+
+                    <div class="card-body">
+                        {!! Form::open(['method' => 'POST', 'route' => ['reset']]) !!}
+                        {!! Form::submit('Reset', ['class' => 'btn btn-primary']) !!}
+                        {!! Form::close() !!}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="container  mb-4">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Probability Settings</h2>
+                <canvas id="probabilityChart"></canvas>
+            </div>
+            <div class="col-md-6">
+                <h2>Actual Rewards</h2>
+                <canvas id="awardedChart"></canvas>
+            </div>
+        </div>
+    </div>
+
+
+@stop
+
+
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
+
+
+@endpush
